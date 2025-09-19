b241db7b5650a5ea3a0120f9e3f41a110d56c0a5
Tantamthan <144194853+Tantamthan@users.noreply.github.com>
2025-09-19 21:32:03 +0700

    Up file

---
+++ Assets/Code/Move.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+
+
+public class Move : MonoBehaviour
+{
+     public float speed = 5f;
+    Rigidbody2D rb;
+    [SerializeField] private bool isFacingRight = true;
+
+
+    private void Start()
+    {
+          rb= GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+       Mover();
+    }
+    void  Mover ()
+    {
+          Vector2 player= new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+       rb.linearVelocity = player.normalized * speed;
+        if (player.x > 0 && !isFacingRight)
+            Flip();
+        else if (player.x < 0 && isFacingRight)
+            Flip();
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight; // đảo hướng
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
+
+
