4432d375a21c885174d97ac559bfc0fd01f245d3
Tantamthan <144194853+Tantamthan@users.noreply.github.com>
2025-09-23 13:39:17 +0700

    SPAWM ENTITY
    tAN,TUAN LAM

--- Assets/Code/SpawmEntity.cs
+++ Assets/Code/SpawmEntity.cs
@@ -1,34 +1,66 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class SpawmEntity : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject enemyprefab;
-    [SerializeField]
-    private float minimunmSpawmTime;
-    [SerializeField]
+    [SerializeField] private GameObject enemyprefab;
+    [SerializeField] private float minimunmSpawmTime = 1f;
+    [SerializeField] private float maximumSpawmTime = 3f;
+    [SerializeField] private Vector2 spawnAreaSize = new Vector2(5f, 5f);
+    [SerializeField] private GameObject spawnEffectPrefab;
 
-    private float maximumSpawmTime;
     private float spawmTime;
-
 
     void Awake()
     {
-        setTimeUntiSpawm();
+        SetTimeUntilSpawm();
     }
 
-    // Update is called once per frame
     void Update()
     {
         spawmTime -= Time.deltaTime;
-        if(spawmTime <= 0)
+        if (spawmTime <= 0)
         {
-            Instantiate(enemyprefab, transform.position, Quaternion.identity);
-            setTimeUntiSpawm();
+            Vector2 spawnPos = GetRandomSpawnPos();
+            StartCoroutine(SpawnWithEffect(spawnPos));
+            SetTimeUntilSpawm();
         }
     }
-    private void setTimeUntiSpawm()
+
+    private void SetTimeUntilSpawm()
     {
         spawmTime = Random.Range(minimunmSpawmTime, maximumSpawmTime);
+    }
+
+    private Vector2 GetRandomSpawnPos()
+    {
+        Vector2 randomPos = new Vector2(
+            Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2),
+            Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2)
+        );
+
+        return (Vector2)transform.position + randomPos;
+    }
+
+    private IEnumerator SpawnWithEffect(Vector2 position)
+    {
+        // Tạo hiệu ứng spawn
+        if (spawnEffectPrefab != null)
+        {
+            GameObject effect = Instantiate(spawnEffectPrefab, position, Quaternion.identity);
+            Destroy(effect, 2f); // xóa hiệu ứng sau 2s
+        }
+
+        // Chờ 2 giây rồi mới spawn quái
+        yield return new WaitForSeconds(2f);
+
+        Instantiate(enemyprefab, position, Quaternion.identity);
+    }
+
+    // Vẽ gizmo trong Scene view để thấy khu vực spawn
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, spawnAreaSize);
     }
 }
---
+++ Assets/Code/test.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public Transform player;
+    private float speed = 3f;
+    [SerializeField] private bool isFacingRight = true;
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Flower();
+    }
+    private void Flower()
+    {
+        Vector2 direction = (player.position - transform.position).normalized;
+    
+        transform.position += (Vector3)(direction * Time.deltaTime * speed);
+        if (direction.x > 0 && !isFacingRight) Flip();
+        else if (direction.x < 0 && isFacingRight) Flip();
+    }
+    void Flip()
+    {
+        isFacingRight = !isFacingRight; // đảo hướng
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
